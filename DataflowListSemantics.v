f3cfd9671999cd9b04114e6a72c74eaef036f2f0
Lionel Rieg <lionel.rieg@ensiie.fr>
2015-07-24 20:01:55 +0200

    Draft of the equivalence of the semantics with forward or backward lists

--- DataflowListSemantics.v
+++ DataflowListSemantics.v
@@ -24,6 +24,22 @@ intros f [| x1 l1] [| x2 l2] y l; simpl; split; intro Heq; decompose [and ex] He
 - subst. inversion_clear H; inversion_clear H0 (* FIXME: generated names *). reflexivity.
 Qed.
 
+Lemma last_app {A} : forall (l l' : list A) d, l' <> nil -> last (l ++ l') d = last l' d.
+Proof.
+intros l l' d Hl'. induction l as [| e l]; simpl.
+- reflexivity.
+- rewrite <- IHl. destruct (l ++ l') eqn:Heq; trivial.
+  apply app_eq_nil in Heq. intuition.
+Qed.
+
+Lemma rev_is_nil {A} : forall l : list A, rev l = nil <-> l = nil.
+Proof.
+intros [| ? ?]; simpl.
++ reflexivity.
++ split; intro H.
+  - apply app_eq_nil in H. intuition discriminate.
+  - discriminate.
+Qed.
 
 (** *  Dataflow semantics with lists  **)
 
@@ -209,22 +225,23 @@ Fixpoint fby (v : const) (s : list value) : list value :=
   | nil => nil
   end.
 
-(* Maybe we should transform "(exists ...) -> ..." into "forall (... -> ...)" *)
+(* Original "(exists ...) -> ..." tranformed into "forall (... -> ...)" *)
 Inductive sem_equation (G : global) : history -> equation -> Prop :=
-  | SEqDef : forall H x cae,
-      (exists v, sem_lexp H (Evar x) v /\ sem_caexp H cae v) ->
+  | SEqDef : forall H x cae v,
+      sem_lexp H (Evar x) v ->
+      sem_caexp H cae v ->
       sem_equation G H (EqDef x cae)
-  | SEqApp : forall H x f arg input output eqs, (* I did not check this one *)
+  | SEqApp : forall H x f arg input output eqs H' vi vo,
       PositiveMap.find f G = Some (mk_node f input output eqs) ->
-      (exists H' vi vo, (* local context (history), input values and output values *)
-          sem_laexp H arg vi (* arg evaluate to vi *)
-       /\ sem_lexp H (Evar x) vo (* x evaluates to vo *)
-       /\ sem_lexp H' (Evar input.(v_name)) vi (* in the local context, input evaluates to vi *)
-       /\ sem_lexp H' (Evar output.(v_name)) vo  (* in the local context, output evaluates to vo *)
-       /\ List.Forall (sem_equation G H') eqs) (* in the local context, the equations have correct semantics *)
-      -> sem_equation G H (EqApp x f arg)
-  | SEqFby : forall H x v y,
-      (exists cl, sem_var H y.(v_name) cl /\ sem_lexp H (Evar x) (fby v cl)) ->
+      sem_laexp H arg vi -> (* arg evaluate to vi *)
+      sem_lexp H (Evar x) vo -> (* x evaluates to vo *)
+      sem_lexp H' (Evar input.(v_name)) vi -> (* in the local context, input evaluates to vi *)
+      sem_lexp H' (Evar output.(v_name)) vo -> (* in the local context, output evaluates to vo *)
+      List.Forall (sem_equation G H') eqs -> (* in the local context, the equations have correct semantics *)
+      sem_equation G H (EqApp x f arg)
+  | SEqFby : forall H x v y cl,
+      sem_var H y.(v_name) cl ->
+      sem_lexp H (Evar x) (fby v cl) ->
       sem_equation G H (EqFby x v y).
 
 (** ***  Reverse orientation for lists: last instant is first  **)
@@ -338,21 +355,103 @@ intros d v s. induction s as [| [| v'] s]; intros n Hhere.
   - specialize (IHs n Hhere). now rewrite fby_rev_next, hold_next, IHs.
 Qed.
 
-(* Maybe we should transform "(exists ...) -> ..." into "forall (... -> ...)" *)
 Inductive sem_equation_rev (G : global) : history -> equation -> Prop :=
-  | SEqDef_rev : forall H x cae,
-      (exists v, sem_lexp H (Evar x) v /\ sem_caexp H cae v) ->
-      sem_equation_rev G H (EqDef x cae)
-  | SEqApp_rev : forall H x f arg input output eqs, (* I did not check this one *)
+  | SEqDef_rev : forall H x cae v,
+      sem_lexp H (Evar x) v -> sem_caexp H cae v -> sem_equation_rev G H (EqDef x cae)
+  | SEqApp_rev : forall H x f arg input output eqs H' vi vo,
       PositiveMap.find f G = Some (mk_node f input output eqs) ->
-      (exists H' vi vo, (* local context (history), input values and output values *)
-          sem_laexp H arg vi (* arg evaluate to vi *)
-       /\ sem_lexp H (Evar x) vo (* x evaluates to vo *)
-       /\ sem_lexp H' (Evar input.(v_name)) vi (* in the local context, input evaluates to vi *)
-       /\ sem_lexp H' (Evar output.(v_name)) vo  (* in the local context, output evaluates to vo *)
-       /\ List.Forall (sem_equation_rev G H') eqs) (* in the local context, the equations have correct semantics *)
-      -> sem_equation_rev G H (EqApp x f arg)
-  | SEqFby_rev : forall H x v y,
-      (exists cl, sem_var H y.(v_name) cl /\ sem_lexp H (Evar x) (fby_rev v cl)) ->
+      sem_laexp H arg vi -> (* arg evaluate to vi *)
+      sem_lexp H (Evar x) vo -> (* x evaluates to vo *)
+      sem_lexp H' (Evar input.(v_name)) vi -> (* in the local context, input evaluates to vi *)
+      sem_lexp H' (Evar output.(v_name)) vo -> (* in the local context, output evaluates to vo *)
+      List.Forall (sem_equation_rev G H') eqs -> (* in the local context, the equations have correct semantics *)
+      sem_equation_rev G H (EqApp x f arg)
+  | SEqFby_rev : forall H x v y cl,
+      sem_var H y.(v_name) cl ->
+      sem_lexp H (Evar x) (fby_rev v cl) ->
       sem_equation_rev G H (EqFby x v y).
 
+
+(** ***  Equivalence between semantics using forward or backward lists  **)
+
+(** First, the equivalence on lesser semantics **)
+
+Lemma for_all2_app {A B} : forall (P : A -> B -> Prop) l1 l2 l1' l2',
+  for_all2 P l1 l1' -> for_all2 P l2 l2' -> for_all2 P (l1 ++ l2) (l1' ++ l2').
+Proof.
+intros P l1 l2. induction l1; intros l1' l2' HP1 HP2.
++ apply for_all2_length in HP1.
+  destruct l1'; simpl in *; assumption || discriminate.
++ destruct l1'.
+  - apply for_all2_length in HP1. simpl in HP1. discriminate.
+  - destruct HP1. split; auto.
+Qed.
+
+Lemma for_all2_rev_aux {A B} : forall (P : A -> B -> Prop) l1 l2, for_all2 P l1 l2 -> for_all2 P (rev l1) (rev l2).
+Proof.
+intros P l1. induction l1; intros l2 Hall.
++ apply for_all2_length in Hall.
+  destruct l2; simpl in *; exact I || discriminate.
++ destruct l2.
+  - apply for_all2_length in Hall. simpl in Hall. discriminate.
+  - destruct Hall. simpl. apply for_all2_app; simpl; auto.
+Qed.
+
+Lemma for_all2_rev {A B} : forall (P : A -> B -> Prop) l1 l2, for_all2 P (rev l1) (rev l2) <-> for_all2 P l1 l2.
+Proof.
+intros. split.
+- setoid_rewrite <- rev_involutive at 5 6. apply for_all2_rev_aux.
+- apply for_all2_rev_aux.
+Qed.
+
+Lemma sem_var_rev : forall H x cl, sem_var (rev H) x (rev cl) <-> sem_var H x cl.
+Proof.
+intros. unfold sem_var. do 2 rewrite map_rev.
+split; intro Heq.
+- now rewrite <- rev_involutive, <- Heq, rev_involutive.
+- unfold venv. now rewrite Heq.
+Qed.
+
+Lemma sem_clock_rev : forall H ce bl, sem_clock (rev H) ce (rev bl) <-> sem_clock H ce bl.
+Proof. intros. setoid_rewrite sem_clock_equiv. apply for_all2_rev. Qed.
+
+Lemma sem_laexp_rev : forall H ce v, sem_laexp (rev H) ce (rev v) <-> sem_laexp H ce v.
+Proof. intros. setoid_rewrite sem_laexp_equiv. apply for_all2_rev. Qed.
+
+Lemma sem_lexp_rev : forall H ce v, sem_lexp (rev H) ce (rev v) <-> sem_lexp H ce v.
+Proof. intros. setoid_rewrite sem_lexp_equiv. apply for_all2_rev. Qed.
+
+Lemma sem_caexp_rev : forall H ce v, sem_caexp (rev H) ce (rev v) <-> sem_caexp H ce v.
+Proof. intros. setoid_rewrite sem_caexp_equiv. apply for_all2_rev. Qed.
+
+Lemma sem_cexp_rev : forall H ce v, sem_cexp (rev H) ce (rev v) <-> sem_cexp H ce v.
+Proof. intros. setoid_rewrite sem_cexp_equiv. apply for_all2_rev. Qed.
+
+Print sem_equation_rev.
+
+Theorem sem_equation_rev_1 : forall G H eq, sem_equation G H eq -> sem_equation_rev G (rev H) eq.
+Proof.
+intros G H eq Heq. induction Heq.
++ econstructor; rewrite sem_lexp_rev || rewrite sem_caexp_rev; eassumption.
++ apply (SEqApp_rev G (rev H) _ _ _ input output eqs (rev H') (rev vi) (rev vo));
+  rewrite ?sem_lexp_rev, ?sem_laexp_rev; trivial.
+  admit. (* TODO: induction scheme is not strong enough *)
++ econstructor.
+  - rewrite sem_var_rev. eassumption. 
+  - now rewrite fby_fby_rev, sem_lexp_rev.
+Qed.
+
+Theorem sem_equation_rev_2 : forall G H eq, sem_equation_rev G (rev H) eq -> sem_equation G H eq.
+Proof.
+intros G H eq. generalize (eq_refl (rev H)). generalize (rev H) at -1.
+intros H' HeqH' Heq. induction Heq.
++ subst. econstructor; rewrite <- sem_lexp_rev || rewrite <- sem_caexp_rev; rewrite rev_involutive; eassumption.
++ apply (SEqApp G H _ _ _ input output eqs (rev H') (rev vi) (rev vo)); trivial; subst;
+  try now rewrite <- sem_lexp_rev || rewrite <- sem_laexp_rev; rewrite rev_involutive.
+  now rewrite sem_lexp_rev || rewrite sem_laexp_rev.
+  now rewrite sem_lexp_rev || rewrite sem_laexp_rev.
+  admit. (* TODO: induction scheme is not strong enough *)
++ subst. econstructor.
+  - rewrite <- sem_var_rev, rev_involutive. eassumption. 
+  - setoid_rewrite <- rev_involutive at 1 2. now rewrite <- fby_fby_rev, sem_lexp_rev, rev_involutive.
+Qed.
