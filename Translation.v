80e84847bec047133a0b80f8d391f56b58df6e1c
Timothy Bourke <tim@tbrk.org>
2015-08-04 08:52:14 +1000

    Simplify the translation functions.
    The monadic definitions with symbol generator and runCompiler do not compose
    well making it tricky to prove inductive correctness lemmas.
    
    For instance, it would be handy to be able to show something like:
    
      Lemma stmt_eval_run_translate_cons:
        forall menv env mems eq eqs menv'' env'',
          stmt_eval menv env
      	      (st_instrs (runCompiler (translate_eqns mems (eq :: eqs))))
      	      (menv'', env'')
          <->
          exists menv' env',
            stmt_eval menv env
      		(st_instrs (runCompiler (translate_eqns mems eqs)))
      		(menv', env')
            /\
            stmt_eval menv' env'
      		(st_instrs (runCompiler (translate_eqn mems eq)))
      		(menv'', env'').
    
    But there are two obstacles:
    
    1. The state used by "translate_eqns eqs" must afterward be passed to
       "translate_eqn eq" (to ensure that symbols are allocated to node
       applications consistently).
    
    2. Given eqns = [ C; B; A ], we would like to produce the stmt:
    
    	Comp
           /    \
          A      Comp
                /    \
               B      Comp
                     /    \
                    C      Skip
    
       But splitting it in two gives:
    
                       Comp
                    .       .
                  .          .
                .             .
           Comp                Comp
          /    \              /    \
         A      Comp         C      Skip
               /    \
              B      Skip
    
       Which is equivalent (outside of stmt_eval (st_instrs ...)), but this
       fact would be tedious (I think) to show. In particular, I did not know
       how to properly express all the (symbolic) components in such a way that
       the lemmas become obvious.
    
       An equivalent form, which would be easier to reason about, is:
    
    		    Comp
    		   /    \
    	       Comp      C
    	      /    \
    	  Comp      B
    	 /    \
         Skip      A
    
       But the generated code is less natural, ((Skip; A); B); C, or there
       is the tedium of switching one form for the other.
    
    We shall see if the functional form is easier to reason about...

--- Translation.v
+++ Translation.v
@@ -8,65 +8,29 @@ Require Import Rustre.DataflowSyntax.
 (** * Translation *)
 
 Record state : Set := mk_state {
-  st_sym : BinNums.positive;  (** symbol generator *)
   st_mem : list ident;        (* m *)
   st_objs : list obj_dec;     (* j *)
   st_instrs : stmt            (* L *)
 }.
 
-Definition Compiler (A : Type): Type
-  := state -> state * A.
+Fixpoint new_objs_eq (symap: (ident * PM.t obj_dec)%type) (eq: equation)
+  : (ident * PM.t obj_dec)%type :=
+  match eq with
+  | EqApp x f _ =>
+    match symap with
+    | (o, map) => (Pos.succ o, PM.add x (mk_obj_dec o f) map)
+    end
+  | _ => symap
+  end.
 
-Definition retc {A}(a: A): Compiler A :=
-  fun s => (s, a).
-Notation "ret!" := (retc).
-
-Definition bind {A B} (c : Compiler A) (f : A -> Compiler B) : Compiler B :=
-  fun s => let (s', a) := c s in
-           f a s'.
-Notation "x <- c ; f" := (bind c (fun x => f))
-    (right associativity, at level 84, c1 at next level).
-
-Definition add_instr (stmt: stmt): Compiler unit :=
-  fun s =>
-    (mk_state s.(st_sym) s.(st_mem)
-              s.(st_objs) (Comp stmt s.(st_instrs)), 
-     tt).
-
-Definition add_mem (m: ident): Compiler unit :=
-  fun s =>
-    (mk_state s.(st_sym) (m :: s.(st_mem))
-              s.(st_objs) s.(st_instrs),
-     tt).
-
-Definition gensym: Compiler ident :=
-  fun s =>
-    let fresh := s.(st_sym) in
-    (mk_state (Pos.succ fresh) s.(st_mem) s.(st_objs) s.(st_instrs),
-     fresh).
-    
-
-Definition add_obj (o: obj_dec): Compiler unit :=
-  fun s => 
-    (mk_state s.(st_sym) 
-              s.(st_mem)
-              (o :: s.(st_objs)) 
-              s.(st_instrs),
-     tt).
-
-Definition new_obj (cls: ident): Compiler ident :=
-  o <- gensym;
-  let obj := mk_obj_dec o cls in
-  _ <- add_obj obj ;
-  ret! o.
-
-Definition runCompiler (c: Compiler unit): state :=
-  let empty_s := mk_state 1 nil nil Skip in
-  fst (c empty_s).
+Definition new_objs (eqns: list equation) : PM.t obj_dec :=
+  snd (List.fold_left new_objs_eq eqns (1%positive, PM.empty obj_dec)).
 
 Section Translate.
 
   Variable memories : PS.t.
+  (* create this using new_objs ? *)
+  (* Variable objects : PM.t obj_dec. *)
 
   Definition tovar (x: ident) : exp :=
     if PS.mem x memories then State x else Var x.
@@ -77,7 +41,7 @@ Section Translate.
     | Con ck x true => Control ck (Ifte (tovar x) s Skip)
     | Con ck x false => Control ck (Ifte (tovar x) Skip s)
     end.
-  
+
   Fixpoint translate_lexp (e: lexp): exp :=
     match e with
     | Econst c => Const c
@@ -99,37 +63,29 @@ Section Translate.
          | CAexp ck e => translate_cexp x e
          end.
 
-  Definition translate_eqn (eqn: equation): Compiler unit :=
+  Definition translate_eqn (eqn: equation): stmt :=
     match eqn with
     | EqDef x (CAexp ck ce) =>
-      let s := Control ck (translate_cexp x ce) in
-      add_instr s
+      Control ck (translate_cexp x ce)
     | EqApp x f (LAexp ck le) =>
-      let c := translate_lexp le in
-      o <- new_obj f;
-        add_instr (Control ck (Step_ap x o c))
+      (* TODO: should the 2nd x reference an object declaration?
+               i.e., objects x*)
+      Control ck (Step_ap x x (translate_lexp le))
     | EqFby x v (LAexp ck le) =>
-      let c := translate_lexp le in
-      let s := Control ck (AssignSt x c) in
-      _ <- add_mem x;
-        add_instr s
+      Control ck (AssignSt x (translate_lexp le))
     end.
 
   (* NB: eqns ordered in reverse order of execution for coherence
          with Is_well_sch. *)
-  Fixpoint translate_eqns (eqns: list equation): Compiler unit :=
-    match eqns with
-    | nil => ret! tt
-    | cons eqn eqns =>
-        _ <- translate_eqn eqn;
-        translate_eqns eqns
-    end.
+  Definition translate_eqns (eqns: list equation): stmt :=
+    List.fold_left (fun i eq => Comp (translate_eqn eq) i) eqns Skip.
 
 End Translate.
 
 Definition translate_node (n: node): class_def :=
   let mems := memories n.(n_eqs) in
-  let s := runCompiler (translate_eqns mems n.(n_eqs)) in
+  let s :=
+      mk_state (PS.elements mems) List.nil (translate_eqns mems n.(n_eqs)) in
   mk_class n.(n_name)
            s.(st_objs)
            (mk_step_fun n.(n_input).(v_name)
@@ -139,7 +95,7 @@ Definition translate_node (n: node): class_def :=
 
 (* Define and translate a simple node. *)
 Section TestTranslate.
-  
+
   Import List.ListNotations.
   Open Scope positive.
   Open Scope list.
@@ -151,12 +107,12 @@ Section TestTranslate.
       EqFby 3 (Cint 0) (LAexp (Con Cbase 1 false) (Evar 2));
       EqDef 2 (CAexp (Con Cbase 1 true) (Eexp (Econst (Cint 7))))
     ].
-  
+
   Definition node1 : node :=
     mk_node 1 (mk_var 1 Cbase) (mk_var 4 Cbase) eqns1.
 
   Eval cbv in (translate_node node1).
- 
+
   Definition prog1 : stmt :=
     Comp (Ifte (Var 1) (Assign 2 (Const (Cint 7)))
                        Skip)
