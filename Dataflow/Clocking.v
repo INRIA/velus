2e64eebc0aa6b9532087f078033358d3e4b8a111
Timothy Bourke <tim@tbrk.org>
2015-11-27 10:24:25 +0100

    No loops in the clocks of well clocked equations.

--- Dataflow/Clocking.v
+++ Dataflow/Clocking.v
@@ -2,10 +2,7 @@
 Require Import Coq.FSets.FMapPositive.
 Require Import Rustre.Common.
 Require Import Rustre.Dataflow.Syntax.
-
 Require Import List.
-Import List.ListNotations.
-Open Scope list_scope.
 
 Definition clockenv := PM.t clock.
 
@@ -15,6 +12,15 @@ Inductive clk_var C (x: ident) ck: Prop :=
 | Cv:
     PM.find x C = Some ck ->
     clk_var C x ck.
+
+Inductive clk_clock C: clock -> Prop :=
+| CCbase:
+    clk_clock C Cbase
+| CCon:
+    forall ck x b,
+      clk_clock C ck ->
+      clk_var C x ck ->
+      clk_clock C (Con ck x b).
 
 Inductive clk_lexp C: lexp -> clock -> Prop :=
 | Cconst:
@@ -67,6 +73,95 @@ Inductive Well_clocked_node C: node -> Prop :=
       clk_var C o Cbase ->
       Well_clocked_node C (mk_node f i o eqs).
 
+Definition Well_clocked_env C : Prop :=
+  forall x ck, PM.find x C = Some ck -> clk_clock C ck.
+
 Definition Well_clocked G : Prop :=
   Forall (fun nd=> exists C, Well_clocked_node C nd) G.
+
+Inductive Has_clock_eq: clock -> equation -> Prop :=
+| HcEqDef: forall x ck ce,
+    Has_clock_eq ck (EqDef x (CAexp ck ce))
+| HcEqApp: forall x f ck le,
+    Has_clock_eq ck (EqApp x f (LAexp ck le))
+| HcEqFby: forall x v0 ck le,
+    Has_clock_eq ck (EqFby x v0 (LAexp ck le)).
+
+(** ** Basic properties of clocking *)
+
+Lemma clk_var_det:
+  forall C x ck1 ck2,
+    clk_var C x ck1
+    -> clk_var C x ck2
+    -> ck1 = ck2.
+Proof.
+  intros C x ck1 ck2.
+  do 2 inversion_clear 1.
+  match goal with
+  | H1: PM.find x C = _, H2: PM.find x C = _ |- _
+    => rewrite H1 in H2; injection H2; now auto
+  end.
+Qed.
+
+Lemma Well_clocked_env_var:
+  forall C x ck,
+    Well_clocked_env C
+    -> clk_var C x ck
+    -> clk_clock C ck.
+Proof.
+  intros C x ck Hwc Hcv.
+  unfold Well_clocked_env in Hwc.
+  inversion_clear Hcv as [Hfv].
+  apply Hwc with (1:=Hfv).
+Qed.
+
+Lemma clk_clock_lexp:
+  forall C le ck,
+    Well_clocked_env C
+    -> clk_lexp C le ck
+    -> clk_clock C ck.
+Proof.
+  induction le as [| |le IH].
+  - inversion_clear 2; now constructor.
+  - intros ck Hwc; inversion_clear 1 as [|? ? Hcv|].
+    apply Well_clocked_env_var with (1:=Hwc) (2:=Hcv).
+  - intros ck Hwc.
+    inversion_clear 1 as [| |? ? ? ck' Hle Hcv].
+    constructor; [now apply IH with (1:=Hwc) (2:=Hle)|assumption].
+Qed.
+
+Lemma clk_clock_cexp:
+  forall C ce ck,
+    Well_clocked_env C
+    -> clk_cexp C ce ck
+    -> clk_clock C ck.
+Proof.
+  induction ce as [i ce1 IH1 ce2 IH2|].
+  - intros ck Hwc.
+    inversion_clear 1 as [? ? ? ? Hcv Hct Hcf|].
+    apply IH1 with (1:=Hwc) in Hct.
+    inversion_clear Hct; assumption.
+  - intros ck Hwc; inversion_clear 1 as [|? ? Hck].
+    apply clk_clock_lexp with (1:=Hwc) (2:=Hck).
+Qed.
+
+Lemma clock_no_loops:
+  forall ck x b,
+    Con ck x b <> ck.
+Proof.
+  induction ck as [|? IH]; [discriminate 1|].
+  injection 1; intros ? ? Heq.
+  apply IH with (1:=Heq).
+Qed.
+
+Lemma clk_clock_sub:
+  forall C ck x b,
+    Well_clocked_env C
+    -> clk_clock C (Con ck x b)
+    -> clk_var C x ck.
+Proof.
+  intros C ck x b Hwc Hclock.
+  inversion_clear Hclock as [|? ? ? Hclock' Hcv'].
+  assumption.
+Qed.
 
---
+++ Dataflow/Clocking/Properties.v
@@ -0,0 +1,86 @@
+Require Import Rustre.Common.
+Require Import Dataflow.Syntax.
+Require Import Dataflow.Clocking.
+Require Import Dataflow.Clocking.Parents.
+
+Require Import Dataflow.IsFree.
+Require Import Dataflow.IsDefined.
+
+Lemma Is_free_in_clock_self_or_parent:
+  forall x ck,
+    Is_free_in_clock x ck
+    -> exists ck' b, ck = Con ck' x b \/ clock_parent (Con ck' x b) ck.
+Proof.
+  Hint Constructors clock_parent.
+  induction ck as [|? IH]; [now inversion 1|].
+  intro Hfree.
+  inversion Hfree as [|? ? ? ? Hfree']; clear Hfree; subst.
+  - exists ck, b; now auto.
+  - specialize (IH Hfree'); clear Hfree'.
+    destruct IH as [ck' [b' Hcp]].
+    exists ck', b'; right.
+    destruct Hcp as [Hcp|Hcp]; [rewrite Hcp| inversion Hcp]; now auto.
+Qed.
+
+Theorem Well_clocked_eq_not_Is_free_in_clock:
+  forall C eq x ck,
+    Well_clocked_env C
+    -> Well_clocked_eq C eq
+    -> Is_defined_in_eq x eq
+    -> Has_clock_eq ck eq
+    -> ~Is_free_in_clock x ck.
+Proof.
+  intros C eq x ck Hwc Hwce Hdef Hhasck Hfree.
+  inversion Hwce as [x' ck' e Hcv Hexp Heq
+                    |x' ck' f e Hcv Hexp Heq
+                    |x' ck' v' e Hcv Hexp];
+    subst; inversion Hdef; inversion Hhasck; clear Hdef Hhasck; subst;
+    pose proof (Well_clocked_env_var _ _ _ Hwc Hcv) as Hclock;
+    apply Is_free_in_clock_self_or_parent in Hfree;
+    destruct Hfree as [ck [b [Hck|Hck]]];
+    (rewrite Hck in *;
+      apply clk_clock_sub with (1:=Hwc) in Hclock;
+      apply clk_var_det with (1:=Hcv) in Hclock;
+      apply clock_no_loops with (1:=Hclock))
+    ||
+    (apply clk_clock_parent with (1:=Hwc) (2:=Hck) in Hclock;
+      apply clk_clock_sub with (1:=Hwc) in Hclock;
+      apply clk_var_det with (1:=Hcv) in Hclock;
+      apply clock_parent_parent' in Hck;
+      rewrite <-Hclock in Hck;
+      apply clock_parent_not_refl with (1:=Hck)).
+Qed.
+
+Corollary Well_clocked_EqDef_not_Is_free_in_clock:
+  forall C x ce ck,
+    Well_clocked_env C
+    -> Well_clocked_eq C (EqDef x (CAexp ck ce))
+    -> ~Is_free_in_clock x ck.
+Proof.
+  intros C x ce ck Hwc Hwce.
+  apply Well_clocked_eq_not_Is_free_in_clock with (1:=Hwc) (2:=Hwce);
+    now constructor.
+Qed.
+
+Corollary Well_clocked_EqApp_not_Is_free_in_clock:
+  forall C x f le ck,
+    Well_clocked_env C
+    -> Well_clocked_eq C (EqApp x f (LAexp ck le))
+    -> ~Is_free_in_clock x ck.
+Proof.
+  intros C x f le ck Hwc Hwce.
+  apply Well_clocked_eq_not_Is_free_in_clock with (1:=Hwc) (2:=Hwce);
+    now constructor.
+Qed.
+
+Corollary Well_clocked_EqFby_not_Is_free_in_clock:
+  forall C x v0 le ck,
+    Well_clocked_env C
+    -> Well_clocked_eq C (EqFby x v0 (LAexp ck le))
+    -> ~Is_free_in_clock x ck.
+Proof.
+  intros C x v0 le ck Hwc Hwce.
+  apply Well_clocked_eq_not_Is_free_in_clock with (1:=Hwc) (2:=Hwce);
+    now constructor.
+Qed.
+
