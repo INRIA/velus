fa67ceffc99bf96e0e212ec62bf85bedfac1dee7
Lélio Brun <lelio.brun@inria.fr>
2016-11-03 12:40:40 +0100

    work on idents

--- Ident.v
+++ Ident.v
@@ -1,5 +1,6 @@
 Require Import Rustre.Common.
 Require Import String.
+Require Import Ascii.
 Require Import List.
 Import List.ListNotations.
 
@@ -14,10 +15,73 @@ Axiom pos_of_str_injective:
   forall x x',
     pos_of_str x = pos_of_str x' ->
     x = x'.
-
+Axiom pos_to_str_not_empty:
+  forall x, pos_to_str x <> ("")%string.
 Axiom pos_to_str_equiv:
   forall x, pos_to_str (pos_of_str x) = x.
-         
+
+Fixpoint In_str (x: ascii) (s: string): Prop :=
+  match s with
+  | EmptyString => False
+  | String x' s => x' = x \/ In_str x s
+  end.
+
+Lemma In_str_eq:
+  forall x s,
+    In_str x (String x s).
+Proof.
+  simpl; now left.
+Qed.
+
+Lemma In_str_app:
+  forall x s1 s2,
+    In_str x (s1 ++ s2)%string <-> In_str x s1 \/ In_str x s2.
+Proof.
+  induction s1; split; simpl; intro H.
+  - now right.
+  - destruct H; auto; contradiction.
+  - destruct H.
+    + now repeat left.
+    + rewrite IHs1 in H; destruct H.
+      * now left; right.
+      * now repeat right.
+  - rewrite IHs1; destruct H as [[|]|].
+    + now left.
+    + now right; left.
+    + now repeat right.
+Qed.
+
+Lemma not_in_str_cons:
+  forall x x' s,
+    ~ In_str x (String x' s) <-> x <> x' /\ ~ In_str x s.
+Proof.
+  split; intro H.
+  - simpl in H; split; intro E; apply H; auto.
+  - simpl; intros [|]; destruct H; auto.
+Qed.
+
+Axiom pos_to_str_valid:
+  forall x, ~ In_str "$" (pos_to_str x).
+
+Lemma append_sep_injectivity:
+  forall x pre1 pre2 post1 post2,
+    ~ In_str x pre1 ->
+    ~ In_str x pre2 ->
+    (pre1 ++ (String x post1) = pre2 ++ (String x post2))%string ->
+    pre1 = pre2 /\ post1 = post2.
+Proof.
+  induction pre1; induction pre2; intros ** In1 In2 Eq.
+  - inv Eq; auto.
+  - apply not_in_str_cons in In2; destruct In2.
+    admit.
+  - apply not_in_str_cons in In1; destruct In1.
+    admit.
+  - apply not_in_str_cons in In1;
+    apply not_in_str_cons in In2;
+    destruct In1; destruct In2.
+    admit.
+Qed.
+
 Module Export Ids <: IDS.
   Definition self := pos_of_str "self".
   Definition out := pos_of_str "out".             
@@ -65,7 +129,7 @@ Module Export Ids <: IDS.
 End Ids.
 
 Definition prefix (pre id: ident) :=
-  pos_of_str (pos_to_str pre ++ "$" ++ pos_to_str id).
+  pos_of_str (pos_to_str pre ++ (String "$" (pos_to_str id))).
 
 Definition prefix_fun (c f: ident): ident :=
   prefix fun_id (prefix c f).
@@ -80,7 +144,11 @@ Proof.
   unfold prefix.
   intros ** H.
   apply pos_of_str_injective in H.
-  admit.
+  apply append_sep_injectivity in H;
+    try apply pos_to_str_valid.
+  destruct H as [H1 H2].
+  apply pos_to_str_injective in H1;
+    apply pos_to_str_injective in H2; auto.
 Qed.
 
 Lemma prefix_fun_injective: 
@@ -125,7 +193,7 @@ Proof.
 Qed.
 
 Definition glob_id (id: ident): ident :=
-  pos_of_str ("$" ++ (pos_to_str id)).
+  pos_of_str (String "$" (pos_to_str id)).
 
 Lemma glob_id_injective:
   forall x x',
@@ -146,7 +214,10 @@ Proof.
   inversion H as [? ? E].
   unfold prefix, glob_id in E.
   apply pos_of_str_injective in E.
-  admit.
+  change (String "$" (pos_to_str x))%string with ("" ++ String "$" (pos_to_str x))%string in E.
+  apply append_sep_injectivity in E; try apply pos_to_str_valid; auto.
+  destruct E as [E]; contradict E.
+  apply pos_to_str_not_empty.
 Qed.
 
 Lemma self_not_prefixed: ~ prefixed self.
@@ -155,6 +226,9 @@ Proof.
   inversion H as [? ? E].
   unfold prefix, self in E.
   apply pos_of_str_injective in E.
-  admit.
+  assert (In_str "$" "self") as Hin
+      by (rewrite <- E, In_str_app; right; apply In_str_eq).
+  destruct Hin as [|[|[|[|]]]]; try discriminate.
+  contradiction.
 Qed.
   
