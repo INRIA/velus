d06715179b2b38b08f078707239a532b0742932d
Timothy Bourke <tim@tbrk.org>
2016-11-16 08:58:24 +0100

    Encore un admit de moins

--- Rustre.v
+++ Rustre.v
@@ -423,19 +423,49 @@ Proof.
   admit.
 Qed.
 
-Lemma NoDup_defs_NoDup_vars_defined:
-  forall eqs,
-    NoDup_defs eqs <-> NoDup (vars_defined eqs).
+Lemma Is_defined_in_vars_defined:
+  forall x eqs,
+    Is_defined_in_eqs x eqs ->
+    In x (vars_defined eqs).
 Proof.
-  unfold vars_defined.
-  induction eqs; split; intro HH.
-  - rewrite concatMap_nil. auto using NoDup.
-  - constructor.
-  - rewrite concatMap_cons.
-    admit.
-  - admit.
+  induction eqs as [|eq eqs]; inversion_clear 1;
+    unfold vars_defined; rewrite concatMap_cons; apply in_app.
+  - left. inv H0; simpl; auto.
+  - intuition.
 Qed.
 
+Lemma NoDup_defs_NoDup_vars_defined:
+  forall eqs,
+    NoDup (vars_defined eqs) ->
+    NoDup_defs eqs.
+Proof.
+  unfold vars_defined.
+  induction eqs as [|eq eqs].
+  - rewrite concatMap_nil; auto using NoDup_defs.
+  - rewrite concatMap_cons.
+    intro Hnodup.
+    apply NoDup_app'_iff in Hnodup.
+    destruct Hnodup as (Hnd1 & Hnd2 & Hni).
+    apply IHeqs in Hnd2.
+    destruct eq.
+    + constructor; auto.
+      simpl in *.
+      intro Hdef.
+      inv Hni. apply H1.
+      now apply Is_defined_in_vars_defined.
+    + constructor; auto.
+      intros x Hin Hdef.
+      simpl in *.
+      apply In_Forall with (1:=Hni) in Hin.
+      apply Hin.
+      now apply Is_defined_in_vars_defined.
+    + constructor; auto.
+      simpl in *.
+      intro Hdef.
+      inv Hni. apply H1.
+      now apply Is_defined_in_vars_defined.
+Qed.
+      
 Lemma ClassIsFusible_translate:
   forall G,
     Well_clocked G ->
