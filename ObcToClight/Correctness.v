00e57528598065e87d6592d1e2b64abded4377f0
Timothy Bourke <tim@tbrk.org>
2017-02-12 18:09:03 +0100

    Extract useful Obc/Clight lemmas into a common file

--- ObcToClight/Correctness.v
+++ ObcToClight/Correctness.v
@@ -17,6 +17,7 @@ Require Import Velus.RMemory.
 Require Import Velus.Ident.
 Require Import Velus.Traces. 
 
+Require Import Velus.ObcToClight.ObcClightCommon.
 Require Import Velus.ObcToClight.MoreSeparation.
 Require Import Velus.ObcToClight.SepInvariant.
 Require Import Velus.ObcToClight.Generation.
@@ -43,8 +44,6 @@ Hint Resolve  Clight.assign_loc_value.
 
 Hint Resolve Z.divide_refl.
 
-
-
 Lemma type_eq_refl:
   forall {A} t (T F: A),
     (if type_eq t t then T else F) = T.
@@ -52,38 +51,6 @@ Proof.
   intros.
   destruct (type_eq t t) as [|Neq]; auto.
   now contradict Neq.
-Qed.
-
-Lemma NoDup_norepet:
-  forall {A} (l: list A),
-    NoDup l <-> list_norepet l.
-Proof.
-  induction l; split; constructor.
-  - now inversion H.
-  - apply IHl; now inversion H.
-  - now inversion H.
-  - apply IHl; now inversion H.
-Qed.
-
-Lemma NoDupMembers_disjoint:
-  forall l1 l2,
-    NoDupMembers (l1 ++ l2) ->
-    list_disjoint (var_names l1) (var_names l2).
-Proof.
-  unfold list_disjoint, var_names.
-  intros l1 l2 H x y Hx Hy.
-  apply in_map_iff in Hx; destruct Hx as ((x', tx) & Ex & Hx);
-    apply in_map_iff in Hy; destruct Hy as ((y', ty) & Ey & Hy);
-      simpl in *; subst.
-  intro E; subst.
-  apply in_split in Hx; destruct Hx as (lx & lx' & Hx);
-    apply in_split in Hy; destruct Hy as (ly & ly' & Hy);
-      subst.
-  rewrite <-app_assoc in H.
-  apply NoDupMembers_app_r in H.
-  rewrite <-app_comm_cons, nodupmembers_cons in H.
-  destruct H as [Notin]; apply Notin.
-  apply InMembers_app; right; apply InMembers_app; right; apply inmembers_eq.
 Qed.
 
 Lemma eq_key_equiv:
