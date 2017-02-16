f6c3cfeaf7edd7d1c6b143ed6fb17842001ad8e0
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2017-02-16 14:40:16 +0100

    Docker runner: make sure that Docker is installed

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 containerId=pedagand/velus
 
+command -v docker >/dev/null 2>&1 || { \
+        echo >&2 "docker is required but could not be found.  Aborting."; \
+        echo >&2 "To setup Docker: https://docs.docker.com/engine/getstarted/step_one/"; \
+        exit 1; }
+
 xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerId`
 docker run -it --rm \
       -e DISPLAY=:0 \
