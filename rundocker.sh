3bbdca30325e6c802b173064fcfde9c4a3c1e8bf
Lélio Brun <lelio.brun@gmail.com>
2019-10-17 19:05:57 +0200

    remove container after execution

--- rundocker.sh
+++ rundocker.sh
@@ -5,4 +5,4 @@ command -v docker >/dev/null 2>&1 || { \
         echo >&2 "To setup Docker: https://docs.docker.com/engine/getstarted/step_one/"; \
         exit 1; }
 
-docker run -it -v $PWD:/home/opam/velus $containerId $1
+docker run -it --rm -v $PWD:/home/opam/velus $containerId $1
