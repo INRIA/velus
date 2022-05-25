219010dcc6eab47b078cb3a3d013bd0979737511
Paul Jeanmaire <paul.jeanmaire@ens.fr>
2022-05-25 17:35:44 +0200

    tttt

--- src/Lustre/Denot/SDtoRel.v
+++ src/Lustre/Denot/SDtoRel.v
@@ -1,10 +1,11 @@
 From Coq Require Import BinPos List.
 From Cpo Require Import Cpo_def Cpo_streams_type Systems.
 
-From Velus Require Import Common Ident Operators Clocks CoindStreams.
+From Velus Require Import Common Ident Environment Operators Clocks CoindStreams.
 From Velus Require Import Lustre.StaticEnv Lustre.LSyntax Lustre.LOrdered Lustre.LSemantics Lustre.LTyping Lustre.LCausality.
 
 Close Scope equiv_scope. (* conflicting notation "==" *)
+Import ListNotations.
 
 Require Import Cpo_ext CommonDS SDfuns Denot Infty.
 
@@ -111,5 +112,116 @@ Proof.
   rewrite <- Eqr, <- Eqx, <- Eqy.
   apply ok_fby1; auto.
 Qed.
+
+
+Definition all_infinite (vars : list ident) (env : DS_prod SI) : Prop :=
+  forall i, In i vars -> infinite (env i).
+
+Definition nprod_inf n (np : nprod n) : Prop.
+  induction n as [|[]]; simpl in *.
+  - exact (infinite np).
+  - exact (infinite np).
+  - exact (infinite (fst np) /\ IHn (snd np)).
+Defined.
+
+Fixpoint nprod_inf' (n : nat) (np : nprod n) {struct n} : Prop :=
+   match n as n return nprod n -> Prop with
+   | 0 => fun np : nprod O => infinite np
+   | S n0 =>
+       match n0 as n0 return nprod (S n0) -> Prop with
+       | O => fun np : nprod 1 => infinite np
+       | S _ => fun np : nprod _ => infinite (fst np)
+                                          /\ nprod_inf' n0 (snd np)
+       end
+   (* | 1 => fun np : nprod 1 => infinite np *)
+   (* | S (S n) => fun np : nprod (S (S n)) => infinite (fst np) *)
+   (*                              /\ nprod_inf' (S n) (snd np) *)
+   (* | S n1 => *)
+   (*     (fun (n2 : nat) (IHn : nprod (S n2) -> Prop) (np0 : nprod (S (S n2))) => *)
+   (*      infinite (fst np0) /\ IHn (snd np0) : Prop) n1 *)
+   (* end) n np *)
+           (*   : forall n : nat, nprod n -> Prop *)
+   end np.
+
+
+
+Definition list_of_nprod n np : nprod_inf n np -> list (Stream svalue).
+  induction n as [|[]]; simpl; intro Hinf.
+  - exact ([S_of_DSv np Hinf]).
+  - exact ([S_of_DSv np Hinf]).
+  - exact ((S_of_DSv (fst np) (proj1 Hinf)) :: IHn _ (proj2 Hinf)).
+Defined.
+
+(* en principe on devrait pouvoir prouver : *)
+Lemma denot_equation_infinite :
+  forall (e : equation) bs,
+    (* + hypothèse de causalité *)
+    let env := FIXP _ (denot_equation e <___> bs) in
+    all_infinite (fst e) env
+    /\ Forall (fun e => nprod_inf _ (denot_exp e env bs)) (snd e).
+Proof.
+  (* la deuxième partie découle facilement de la première *)
+Admitted.
+
+Definition streams_of_env vars env (Hinf : all_infinite vars env) : list (ident * Stream svalue).
+  (* refine (List.map (fun x => (x, S_of_DSv (env x) (Hinf x _))) vars). *)
+  induction vars as [| x vars].
+  - exact [].
+  - apply List.cons.
+    + exact (x, S_of_DSv (env x) (Hinf x (or_introl eq_refl))).
+    + apply IHvars. firstorder.
+Defined.
+
+Definition hist_of_env vars env Hinf : Str.history :=
+  Env.from_list (streams_of_env vars env Hinf).
+
+Definition ok_var :
+  forall vars env Hinf x (Hin : In x vars),
+    sem_var (hist_of_env vars env Hinf) x (S_of_DSv (env x) (Hinf x Hin)).
+Proof.
+  intros.
+  econstructor. (* 2: reflexivity. *)
+  apply Env.find_2, Env.find_In_from_list.
+Abort.
+
+
+
+Definition list_of_nprod' n (np : nprod n) : list (DS (sampl value)).
+  revert dependent n.
+  fix f 1; intros.
+  destruct n as [|n].
+  - exact [].
+  - specialize (f n).
+    destruct n.
+    + exact [np].
+    + exact (f (snd np)).
+Defined.
+
+(* Fixpoint list_of_nprod n (np : nprod n) {struct n} : list (DS (sampl value)) := *)
+(*   match n, np with *)
+(*   | O, _ => [] *)
+(*   | S O, s => [s] *)
+(*   | S n, (s,t) => s :: list_of_nprod n t *)
+(*   end. *)
+
+Lemma test :
+  forall PSyn prefs (G : @global PSyn prefs),
+  forall (e : equation) bs (bsi : infinite bs),
+    let env := FIXP _ (denot_equation e <___> bs) in
+    forall Henv : all_infinite (fst e) env,
+      (* TODO: all_safe *)
+    let H := hist_of_env (fst e) env Henv in
+    sem_equation G (H, Env.empty _) (S_of_DS id bs bsi) e.
+Proof.
+  Opaque denot_equation.
+  intros.
+  destruct e as (xs,es).
+  (* pose (ss := denot_exps es env0 bs). *)
+  pose (ss := List.map (fun e => list_of_nprod' _ (denot_exp e env0 bs)) es).
+  econstructor; simpl in *.
+  Search denot_equation.
+Qed.
+
+
 
 End SDTOREL.
