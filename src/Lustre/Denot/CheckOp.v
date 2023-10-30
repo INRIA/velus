b60126fdbaa3128181984bce3492cab9935d92d9
Paul Jeanmaire <paul.jeanmaire@ens.fr>
2023-10-30 14:21:09 +0100

    check_binop simpliste

--- src/Lustre/Denot/CheckOp.v
+++ src/Lustre/Denot/CheckOp.v
@@ -27,6 +27,10 @@ Module Type CHECKOP
        (Import Den   : LDENOT        Ids Op OpAux Cks Senv Syn Lord)
        (Import OpErr : OP_ERR        Ids Op OpAux Cks Senv Syn Lord Den).
 
+
+(* 1ère analyse, très simpliste, qui ne dépend pas des valeurs *)
+Module TEST1.
+
 (** caractérise un opérateur unaire qui n'échoue jamais sur un
     type de valeur donné *)
 Parameter check_unop : unop -> type -> bool.
@@ -36,8 +40,18 @@ Conjecture check_unop_correct :
     forall v, wt_value v ty ->
          sem_unop op v ty <> None.
 
-  (* true -> cannot fail
-   false -> we don't know *)
+(** caractérise un opérateur binaire qui n'échoue jamais sur un
+    type de valeur donné *)
+Parameter check_binop : binop -> type -> type -> bool.
+Conjecture check_binop_correct :
+  forall op ty1 ty2,
+    check_binop op ty1 ty2 = true ->
+    forall v1 v2, wt_value v1 ty1 ->
+             wt_value v2 ty2 ->
+             sem_binop op v1 ty1 v2 ty2 <> None.
+
+(* true -> cannot fail
+ * false -> we don't know *)
 Fixpoint check_exp (e : exp) : bool :=
   match e with
   | Econst _ => true
@@ -46,10 +60,14 @@ Fixpoint check_exp (e : exp) : bool :=
   | Elast _ _ => false (* restr *)
   | Eunop op e ann =>
       match typeof e with
-      | [ty] => check_exp e && check_unop op ty
+      | [ty] => check_unop op ty && check_exp e
       | _ => false
       end
-  | Ebinop op e1 e2 ann => false
+  | Ebinop op e1 e2 ann =>
+      match typeof e1, typeof e2 with
+      | [ty1], [ty2] => check_binop op ty1 ty2 && check_exp e1 && check_exp e2
+      | _,_ => false
+      end
   | Eextcall _ _ _ => false (* restr *)
   | Efby e0s es ann => forallb check_exp e0s && forallb check_exp es
   | Earrow e0s es ann => false (* restr *)
@@ -83,6 +101,20 @@ Proof.
     apply DSForall_all; intros [| v |]; auto.
     intro Wtv.
     now apply check_unop_correct.
+  - (* Ebinop *)
+    destruct (typeof e1) as [|ty1 []] eqn:Hty1, (typeof e2) as [|ty2 []] eqn:Hty2; try congruence.
+    apply andb_prop in Hchk as [FF1 F3].
+    apply andb_prop in FF1 as [F1 F2].
+    constructor; auto.
+    (* c'est très très simple *)
+    intros ty1' ty2' Hty1' Hty2'.
+    rewrite Hty1' in Hty1; inv Hty1.
+    rewrite Hty2' in Hty2; inv Hty2.
+    intros ss1 ss2.
+    apply DSForall_all.
+    intros [[|v1|] [|v2|]]; auto.
+    intros Wt1 Wt2.
+    now apply check_binop_correct.
   - (* Efby *)
     apply andb_prop in Hchk as [F1 F2].
     apply forallb_Forall in F1, F2.
@@ -111,5 +143,6 @@ Proof.
     constructor; simpl_Forall; auto.
 Qed.
 
-End CHECKOP.
+End TEST1.
 
+End CHECKOP.
