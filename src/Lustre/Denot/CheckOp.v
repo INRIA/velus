e4e46c4e19fcff8661e31ece139884dcb2441c3f
Paul Jeanmaire <paul.jeanmaire@ens.fr>
2023-10-30 18:09:34 +0100

    plan pour analyse 2

--- src/Lustre/Denot/CheckOp.v
+++ src/Lustre/Denot/CheckOp.v
@@ -28,6 +28,110 @@ Module Type CHECKOP
        (Import OpErr : OP_ERR        Ids Op OpAux Cks Senv Syn Lord Den).
 
 
+Module TEST2.
+
+  (* TODO: 2 résultats
+     - mq que l'analyse renvoie une sur-approx :
+          Any ---> pas err error_Op
+          Val v ---> si pres v' alors v' = v
+          Err  ---> on s'en fout ?
+     - mq ça donne op_correct ??
+   *)
+
+Inductive res : Type :=
+| Any
+| Err
+| Val (v : value).
+
+Parameter join : res -> res -> res.
+  (* fun r1 r2 => match r1, r2 with *)
+  (*           | Err, _ | _, Err => Err *)
+  (*           | _, _ => Any *)
+  (*           end. *)
+
+Parameter check_unop : unop -> res -> type -> res.
+Conjecture check_unop_correct :
+  forall op r ty,
+    check_unop op r ty <> Err ->
+    sem_unop op r ty <> None.
+
+Parameter check_binop : binop -> res -> type -> res -> type -> res.
+
+Fixpoint check_exp (e : exp) : list res :=
+  match e with
+  | Econst c => [ Val (Vscalar (sem_cconst c)) ]
+  | Eenum _ _ => [ Err ] (* TODO ?? *)
+  | Evar _ _ => [ Any ]
+  | Elast _ _ => [ Err ] (* restr *)
+  | Eunop op e ann =>
+      match typeof e with
+      | [ty] =>
+          match check_exp e with
+          | [ v ] => [ check_unop op v ty ]
+          | _ => [ Err ]
+          end
+      | _ => [ Err ]
+      end
+  | Ebinop op e1 e2 ann =>
+      match typeof e1, typeof e2 with
+      | [ty1], [ty2] =>
+          match check_exp e1, check_exp e2 with
+          | [ v1 ], [ v2 ] => [ check_binop op v1 ty1 v2 ty2 ]
+          | _, _ => [ Err ]
+          end
+      | _,_ => [ Err ]
+      end
+  | Eextcall _ _ _ => [ Err ] (* restr *)
+  | Efby e0s es ann =>
+      map2 join
+      (concat (List.map check_exp e0s))
+      (concat (List.map check_exp es))
+
+  | Earrow e0s es ann => [ Err ] (* restr *)
+  | Ewhen es _ t ann =>
+      (* on ignore la valeur de la condition *)
+      concat (List.map check_exp es)
+  | Emerge _ ess (tys,_) =>
+      fold_left (map2 join)
+        (List.map (fun '(t, es) => concat (List.map check_exp es)) ess)
+        (List.map (fun _ => Any) tys) (* vrai problème : ça annihile tout espoir de Val *)
+  (* | Ecase e ess None ann => check_exp e && forallb (fun '(t,es) => forallb check_exp es) ess *)
+  (* | Ecase e ess (Some des) ann => check_exp e && forallb (fun '(t,es) => forallb check_exp es) ess && forallb check_exp des *)
+  (* | Eapp f es er ann => forallb check_exp es && forallb check_exp er *)
+  | _ => [ Err ]
+  end.
+
+Theorem check_exp_correct :
+  forall G ins envG envI bs env,
+  forall e, Forall (fun v => v <> Err) (check_exp e) ->
+       op_correct_exp G ins envG envI bs env e.
+Proof.
+  intros *.
+  induction e using exp_ind2; simpl; intro Hchk; try now inv Hchk.
+  - (* Econst *)
+    constructor.
+  - (* Evar *)
+    constructor.
+  - (* Eunop *)
+    destruct (typeof e) as [|ty []] eqn:Hty; try now inv Hchk.
+    destruct (check_exp e) as [|v []] eqn:Hv; try now inv Hchk.
+    inv Hchk.
+    constructor. apply IHe; constructor; auto.
+
+    apply andb_prop in Hchk as [F1 F2].
+    constructor; auto.
+    intros ty' Hty'.
+    rewrite Hty' in Hty; inv Hty.
+    (* c'est très très simple *)
+    apply nprod_forall_Forall, Forall_forall.
+    intros s Hin.
+
+
+Qed.
+
+End TEST2.
+
+
 (* 1ère analyse, très simpliste, qui ne dépend pas des valeurs *)
 Module TEST1.
 
