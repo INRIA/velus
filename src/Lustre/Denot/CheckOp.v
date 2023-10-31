7080f01ba2405e2cf6b90bb164a712dfee123663
Paul Jeanmaire <paul.jeanmaire@ens.fr>
2023-10-31 17:17:25 +0100

    test2 après parce que ça ne va pas fonctionner

--- src/Lustre/Denot/CheckOp.v
+++ src/Lustre/Denot/CheckOp.v
@@ -28,114 +28,6 @@ Module Type CHECKOP
        (Import OpErr : OP_ERR        Ids Op OpAux Cks Senv Syn Lord Den).
 
 
-Module TEST2.
-
-  (* TODO: 2 résultats
-     - mq que l'analyse renvoie une sur-approx :
-          Any ---> pas err error_Op
-          Val v ---> si pres v' alors v' = v
-          Err  ---> on s'en fout ?
-     - mq ça donne op_correct ??
-   *)
-
-Inductive res : Type :=
-| Any
-| Err
-| Val (v : value).
-
-Parameter join : res -> res -> res.
-  (* fun r1 r2 => match r1, r2 with *)
-  (*           | Err, _ | _, Err => Err *)
-  (*           | _, _ => Any *)
-  (*           end. *)
-
-Parameter check_unop : unop -> res -> type -> res.
-Conjecture check_unop_correct :
-  forall op r ty,
-    check_unop op r ty <> Err ->
-    match r with
-    | Val v => sem_unop op v ty <> None
-    | _ =>  forall v, wt_value v ty ->
-                sem_unop op v ty <> None
-    end.
-
-Parameter check_binop : binop -> res -> type -> res -> type -> res.
-
-Fixpoint check_exp (e : exp) : list res :=
-  match e with
-  | Econst c => [ Val (Vscalar (sem_cconst c)) ]
-  | Eenum _ _ => [ Err ] (* TODO ?? *)
-  | Evar _ _ => [ Any ]
-  | Elast _ _ => [ Err ] (* restr *)
-  | Eunop op e ann =>
-      match typeof e with
-      | [ty] =>
-          match check_exp e with
-          | [ v ] => [ check_unop op v ty ]
-          | _ => [ Err ]
-          end
-      | _ => [ Err ]
-      end
-  | Ebinop op e1 e2 ann =>
-      match typeof e1, typeof e2 with
-      | [ty1], [ty2] =>
-          match check_exp e1, check_exp e2 with
-          | [ v1 ], [ v2 ] => [ check_binop op v1 ty1 v2 ty2 ]
-          | _, _ => [ Err ]
-          end
-      | _,_ => [ Err ]
-      end
-  | Eextcall _ _ _ => [ Err ] (* restr *)
-  | Efby e0s es ann =>
-      map2 join
-      (concat (List.map check_exp e0s))
-      (concat (List.map check_exp es))
-
-  | Earrow e0s es ann => [ Err ] (* restr *)
-  | Ewhen es _ t ann =>
-      (* on ignore la valeur de la condition *)
-      concat (List.map check_exp es)
-  | Emerge _ ess (tys,_) =>
-      fold_left (map2 join)
-        (List.map (fun '(t, es) => concat (List.map check_exp es)) ess)
-        (List.map (fun _ => Any) tys) (* vrai problème : ça annihile tout espoir de Val *)
-  (* | Ecase e ess None ann => check_exp e && forallb (fun '(t,es) => forallb check_exp es) ess *)
-  (* | Ecase e ess (Some des) ann => check_exp e && forallb (fun '(t,es) => forallb check_exp es) ess && forallb check_exp des *)
-  (* | Eapp f es er ann => forallb check_exp es && forallb check_exp er *)
-  | _ => [ Err ]
-  end.
-
-Theorem check_exp_correct :
-  forall G ins envG envI bs env,
-  forall e, Forall (fun v => v <> Err) (check_exp e) ->
-       op_correct_exp G ins envG envI bs env e.
-Proof.
-  intros *.
-  induction e using exp_ind2; simpl; intro Hchk; try now inv Hchk.
-  - (* Econst *)
-    constructor.
-  - (* Evar *)
-    constructor.
-  - (* Eunop *)
-    destruct (typeof e) as [|ty []] eqn:Hty; try now inv Hchk.
-    destruct (check_exp e) as [|v []] eqn:Hv; try now inv Hchk.
-    inv Hchk.
-    constructor. apply IHe; constructor; auto.
-
-    apply andb_prop in Hchk as [F1 F2].
-    constructor; auto.
-    intros ty' Hty'.
-    rewrite Hty' in Hty; inv Hty.
-    (* c'est très très simple *)
-    apply nprod_forall_Forall, Forall_forall.
-    intros s Hin.
-
-
-Qed.
-
-End TEST2.
-
-
 (* 1ère analyse, très simpliste, qui ne dépend pas des valeurs *)
 Module TEST1.
 
@@ -252,5 +144,216 @@ Proof.
 Qed.
 
 End TEST1.
+
+
+Module TEST2.
+
+  (* TODO: 2 résultats
+     - mq que l'analyse renvoie une sur-approx :
+          Any ---> pas err error_Op
+          Val v ---> si pres v' alors v' = v
+          Err  ---> on s'en fout ?
+     - mq ça donne op_correct ??
+   *)
+
+Inductive res : Type :=
+| Any
+| Err
+| Val (v : value).
+
+Parameter join : res -> res -> res.
+  (* fun r1 r2 => match r1, r2 with *)
+  (*           | Err, _ | _, Err => Err *)
+  (*           | _, _ => Any *)
+  (*           end. *)
+
+
+(* true => ok, false => échoue, cf. TEST1 *)
+Parameter check_unop_any : unop -> type -> bool.
+
+Conjecture check_unop_any_correct :
+  forall op ty,
+    check_unop_any op ty = true ->
+    forall v, wt_value v ty ->
+         sem_unop op v ty <> None.
+
+(* Some v => résultat, None => échoue *)
+Definition check_unop_val : unop -> value -> type -> option value := sem_unop.
+
+Definition check_unop op r ty :=
+  match r with
+  | Any => if check_unop_any op ty then Any else Err
+  | Val v => match check_unop_val op v ty with
+            | Some v' => Val v'
+            | None => Err
+            end
+  | Err => Err
+  end.
+
+(* Conjecture check_unop_val_spec : *)
+(*   forall op v ty, match check_unop_val op v ty with *)
+(*              | Val v' => sem_unop op v ty = Some v' *)
+(*              | Any => forall v, wt_value v ty ->  *)
+(*              end. *)
+
+(* Conjecture check_unop_correct : *)
+(*   forall op r ty, *)
+(*     check_unop op r ty <> Err -> *)
+(*     match r with *)
+(*     | Val v => sem_unop op v ty <> None *)
+(*     | _ =>  forall v, wt_value v ty -> *)
+(*                 sem_unop op v ty <> None *)
+(*     end. *)
+
+Parameter check_binop : binop -> res -> type -> res -> type -> res.
+
+Fixpoint check_exp (e : exp) : list res :=
+  match e with
+  | Econst c => [ Val (Vscalar (sem_cconst c)) ]
+  | Eenum _ _ => [ Err ] (* TODO ?? *)
+  | Evar _ _ => [ Err ]
+  | Elast _ _ => [ Err ] (* restr *)
+  | Eunop op e ann =>
+      match typeof e with
+      | [ty] =>
+          match check_exp e with
+          | [ r ] => [ check_unop op r ty ]
+          | _ => [ Err ]
+          end
+      | _ => [ Err ]
+      end
+  | Ebinop op e1 e2 ann =>
+      match typeof e1, typeof e2 with
+      | [ty1], [ty2] =>
+          match check_exp e1, check_exp e2 with
+          | [ v1 ], [ v2 ] => [ check_binop op v1 ty1 v2 ty2 ]
+          | _, _ => [ Err ]
+          end
+      | _,_ => [ Err ]
+      end
+  | Eextcall _ _ _ => [ Err ] (* restr *)
+  | Efby e0s es ann =>
+      map2 join
+      (concat (List.map check_exp e0s))
+      (concat (List.map check_exp es))
+
+  | Earrow e0s es ann => [ Err ] (* restr *)
+  | Ewhen es _ t ann =>
+      (* on ignore la valeur de la condition *)
+      concat (List.map check_exp es)
+  | Emerge _ ess (tys,_) =>
+      fold_left (map2 join)
+        (List.map (fun '(t, es) => concat (List.map check_exp es)) ess)
+        (List.map (fun _ => Any) tys) (* vrai problème : ça annihile tout espoir de Val *)
+  (* | Ecase e ess None ann => check_exp e && forallb (fun '(t,es) => forallb check_exp es) ess *)
+  (* | Ecase e ess (Some des) ann => check_exp e && forallb (fun '(t,es) => forallb check_exp es) ess && forallb check_exp des *)
+  (* | Eapp f es er ann => forallb check_exp es && forallb check_exp er *)
+  | _ => [ Err ]
+  end.
+
+
+(*XXXXXXXXXXX pris dans Safe.v  *)
+Definition ty_DS (ty : type) (s : DS (sampl value)) : Prop :=
+  DSForall_pres (fun v => wt_value v ty) s.
+Definition safe_op {A} : DS (sampl A) -> Prop :=
+  DSForall (fun v => match v with err error_Op => False | _ => True end).
+Lemma safe_op_sunop :
+  forall op s tye,
+    safe_op s ->
+    DSForall_pres (fun v => sem_unop op v tye <> None) s ->
+    safe_op (sunop (fun v => sem_unop op v tye) s).
+Admitted.
+(* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX *)
+
+
+Theorem check_exp_approx :
+  forall G ins envG envI bs env,
+  forall e,
+    Forall2
+      (fun r s => match r with
+               | Any => safe_op s (* /\ ty_DS (typeof e) s *)
+               | Val v => DSForall_pres (fun v' => v' = v) s
+               | Err => True
+               end)
+      (check_exp e)
+      (list_of_nprod (denot_exp G ins e envG envI bs env)).
+Proof.
+  intros *.
+  induction e using exp_ind2.
+  - (* Econst *)
+    constructor; auto.
+    rewrite denot_exp_eq.
+    unfold DSForall_pres, sconst.
+    apply DSForall_map, DSForall_all.
+    intro; cases_eqn HH.
+  - (* Eenum *)
+    constructor; auto.
+  - (* Evar *)
+    (* TODO: plus de boulôt ici si on veut supposer Any *)
+    constructor; auto.
+  - (* Elast *)
+    constructor; auto.
+  - (* Eunop *)
+    rewrite denot_exp_eq.
+    revert IHe.
+    gen_sub_exps.
+    destruct (numstreams e) as [|[]] eqn:HH; intros; simpl.
+    1,3: cases_eqn HH; apply Forall2_length in IHe;
+      rewrite list_of_nprod_length in IHe; simpl in IHe; lia.
+    destruct (typeof e) as [|ty []].
+    1,3: constructor; auto.
+    inv IHe; inv H3.
+    constructor; auto.
+    repeat (cases_eqn HH; simpl in *; subst); try congruence.
+    + (* c'est très très simple *)
+      apply safe_op_sunop; auto.
+      apply DSForall_all; intros [| v |]; auto.
+      (* intro Wtv. *)
+      apply check_unop_any_correct; auto.
+
+Qed.
+
+
+Theorem check_exp_correct :
+  forall G ins envG envI bs env,
+  forall e, Forall (fun v => v <> Err) (check_exp e) ->
+       op_correct_exp G ins envG envI bs env e.
+Proof.
+  intros *.
+  induction e using exp_ind2; simpl; intro Hchk; try now inv Hchk.
+  - (* Econst *)
+    constructor.
+  - (* Evar *)
+    constructor.
+  - (* Eunop *)
+    destruct (typeof e) as [|ty []] eqn:Hty; try now inv Hchk.
+    destruct (check_exp e) as [|v []] eqn:Hv; try now inv Hchk.
+    inv Hchk.
+    constructor.
+    + apply IHe; constructor; auto.
+      intro; subst; rewrite check_unop_err in *; contradiction.
+    + intros ty' Hty'.
+      apply check_unop_correct in H1.
+
+
+
+
+
+      apply nprod_forall_Forall, Forall_forall.
+      intros s Hin.
+
+
+      apply andb_prop in Hchk as [F1 F2].
+    constructor; auto.
+    intros ty' Hty'.
+    rewrite Hty' in Hty; inv Hty.
+    (* c'est très très simple *)
+    apply nprod_forall_Forall, Forall_forall.
+    intros s Hin.
+
+
+Qed.
+
+End TEST2.
 
 End CHECKOP.
